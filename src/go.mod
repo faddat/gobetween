287bf36589d3bad8bf55d737157a87f50a17ab26
Illarion Kovalchuk <illarion.kovalchuk@gmail.com>
2021-09-04 21:43:40 +0300

    Updated dependencies and fixed proxyprotocol incompatibility

--- src/go.mod
+++ src/go.mod
@@ -1,6 +1,6 @@
 module github.com/yyyar/gobetween
 
-go 1.14
+go 1.17
 
 require (
 	github.com/armon/go-metrics v0.3.3 // indirect
--- src/utils/proxyprotocol/proxyprotocol.go
+++ src/utils/proxyprotocol/proxyprotocol.go
@@ -3,13 +3,12 @@ package proxyprotocol
 import (
 	"fmt"
 	"net"
-	"strconv"
 
 	proxyproto "github.com/pires/go-proxyproto"
 )
 
-func addrToIPAndPort(addr net.Addr) (ip net.IP, port uint16, err error) {
-	ipString, portString, err := net.SplitHostPort(addr.String())
+func getAddrIp(addr net.Addr) (ip net.IP, err error) {
+	ipString, _, err := net.SplitHostPort(addr.String())
 	if err != nil {
 		return
 	}
@@ -20,33 +19,22 @@ func addrToIPAndPort(addr net.Addr) (ip net.IP, port uint16, err error) {
 		return
 	}
 
-	p, err := strconv.ParseInt(portString, 10, 64)
-	if err != nil {
-		return
-	}
-	port = uint16(p)
 	return
 }
 
 /// SendProxyProtocolV1 sends a proxy protocol v1 header to initialize the connection
 /// https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt
 func SendProxyProtocolV1(client net.Conn, backend net.Conn) error {
-	sourceIP, sourcePort, err := addrToIPAndPort(client.RemoteAddr())
-	if err != nil {
-		return err
-	}
-
-	destinationIP, destinationPort, err := addrToIPAndPort(client.LocalAddr())
-	if err != nil {
-		return err
-	}
 
 	h := proxyproto.Header{
-		Version:            1,
-		SourceAddress:      sourceIP,
-		SourcePort:         sourcePort,
-		DestinationAddress: destinationIP,
-		DestinationPort:    destinationPort,
+		Version:         1,
+		SourceAddr:      client.RemoteAddr(),
+		DestinationAddr: client.LocalAddr(),
+	}
+
+	sourceIP, err := getAddrIp(client.RemoteAddr())
+	if err != nil {
+		return err
 	}
 	if sourceIP.To4() != nil {
 		h.TransportProtocol = proxyproto.TCPv4
